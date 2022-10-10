dd8be2ee178bacca8b1d973331f2fd81aea8dbaa
Gabriel Hernandes <gabrielmh13@hotmail.com>
2022-10-10 09:06:35 -0300

    Finalizado

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import { Header } from "./components/Header"
+
+import './global.css'
+
+import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar"
+import { Post } from "./components/Post"
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/gabrielmh13.png',
+      name: 'Gabriel Hernandes',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-10-05 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/github.png',
+      name: 'GitHub',
+      role: 'Company'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-10-02 10:00:00')
+  }
+]
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default App
